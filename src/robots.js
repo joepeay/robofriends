d40fdf6aff81b41fae77379851ee6f00a108adc4
Joe Peay <joseph.peay@hotelengine.com>
2023-07-01 18:25:42 -0600

    get robofriends online

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+{
+    id: 1,
+    name: 'Joe da Pub',
+    username: 'daPub',
+    email: 'pub@mail.com'
+},
+{
+    id: 2,
+    name: 'Whacka Doo',
+    username: 'whacky',
+    email: 'whack@mail.com'
+},
+{
+    id: 3,
+    name: 'Shemp Stooge',
+    username: 'shemp',
+    email: 'shemp@mail.com'
+},
+{
+    id: 4,
+    name: 'Ralph E. Boy',
+    username: 'cramden',
+    email: 'ralphie@mail.com'
+},
+{
+    id: 5,
+    name: 'Snarf Snarf',
+    username: 'snarf',
+    email: 'snarf@mail.com'
+},
+{
+    id: 6,
+    name: 'Who Dat',
+    username: 'whodat',
+    email: 'whodat@mail.com'
+},
+{
+    id: 7,
+    name: 'Poo Citti',
+    username: 'stank',
+    email: 'pu@mail.com'
+},
+];
